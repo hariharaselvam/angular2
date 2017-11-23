b4fca78cd8fb426c98a504ec237e778ab416163e
hariharaselvam <hariharaselvam8@gmail.com>
2017-11-23 21:07:35 +0530

    new component

--- my4-app_ang-cli/src/app/app.component.ts
+++ my4-app_ang-cli/src/app/app.component.ts
@@ -1,19 +1,42 @@
 
+import {Component} from '@angular/core';
 
-
-
-import { Component } from '@angular/core';
-
+export class TvChannel {
+  id: number;
+  name: string;
+  programName: string;
+}
 
 @Component({
-    selector: 'app-root',
-  template: '<div>Most popular JS lib: {{lib}}.<br><br> <button (click)="whichLib()">Vote</button></div>'
+  selector: 'app-root',
+  styles:[`  section {width: 34em; 
+                      margin: 2em; 
+                      padding: 1em; 
+                      border: 1px dotted orange;}
+         `],
+
+  template: `
+            <section>
+                 <h2>{{title}}: {{chan.name}}</h2> 
+
+                 <aside> Showing {{chan.programName}}</aside> 
+                 <p>See on channel: {{chan.id}} - TataSky </p>
+                <div>
+                      <label>Current Program: </label>
+                      <input [(ngModel)]="chan.programName" placeholder="My fav prog">
+                </div>
+            </section>
+            
+             `
 })
 
 
 export class AppComponent {
- public lib = "jQuery" ;
+  private title = 'Popular TV Channel';
 
-  //whichLib() { console.log('Voted - lib: ', this.lib)};
-    public whichLib = () => { console.log('Voted - library: ', this.lib)};
+  private chan: TvChannel = {
+    id: 602,
+    name: 'Republic' ,
+    programName: "News"
+  };
 }
